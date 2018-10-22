2a54fb0286051a0c52800ec826f4fde0b333b9b7
Phil Hartley <philh725@gmail.com>
2018-10-22 03:01:26 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,6 +2,8 @@
 def position_taken?(board, index)
   if board[index] == " " or board[index] == ""
     false
+  elsif board[index] == nil
+    false
   elsif board[index] == "O" or board[index] == "X"
     true
   end
